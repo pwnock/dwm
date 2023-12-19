2bfaecd306ad7841c309806f997104b17ffc32fe
pwnock <lukem@disroot.org>
2023-12-19 03:02:13 +0000

    changed more stuff in config

--- config.h
+++ config.h
@@ -28,7 +28,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Pale Moon",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Pale Moon",  NULL,       NULL,       1 << 5,       True,           -1 },
 };
 
 /* layout(s) */
